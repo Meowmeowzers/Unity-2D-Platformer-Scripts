e060ae3aa3d41a4ad017df88d5cc76ca6076da62
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2022-04-16 00:33:12 +0800

    New Backup

--- CameraFollow.cs
+++ CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform player;
-
+    
     private void Update()
     {
         transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
